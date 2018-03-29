00342fc90191f4c2f330b5290332cefffa9800e4
Zac Guo <zacharyg@usc.edu>
2018-03-29 14:47:29 -0700

    added function for sending email invite

--- FinalProject/app/src/main/java/edu/usc/cs404/catchup/EmailActivity.java
+++ FinalProject/app/src/main/java/edu/usc/cs404/catchup/EmailActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 
 public class EmailActivity extends AppCompatActivity {
 
@@ -20,7 +23,7 @@ public class EmailActivity extends AppCompatActivity {
 
         //print email for debugging purposes
         Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-
+        sendEmail(result);
         //CHECK database to see if email exists. if so, add friends!
         try {
             //Code for checking DB
@@ -29,10 +32,35 @@ public class EmailActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             //Here we can display a fail message and invite the friend to the app
+            sendEmail(result);
             Toast.makeText(getApplicationContext(),
                     "Invite to Catchup sent!",
                     Toast.LENGTH_LONG).show();
             e.printStackTrace();
+        }
+    }
+
+    protected void sendEmail(String email) {
+
+        String[] TO = {email};
+//        String[] CC = {"xyz@gmail.com"};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your Friend has invited you to Catchup!");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(EmailActivity.this,
+                    "There is no email client installed.", Toast.LENGTH_SHORT).show();
         }
     }
 }
