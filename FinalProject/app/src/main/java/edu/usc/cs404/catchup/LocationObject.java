5907b76cb4715034df8820f4427ae1a6bbe2ac2d
Nikkan Ghosh <nikkangh@usc.edu>
2018-04-11 14:09:55 -0700

    datamodel finalized. listadapter modified. image yet to be added.

--- FinalProject/app/src/main/java/edu/usc/cs404/catchup/LocationObject.java
+++ FinalProject/app/src/main/java/edu/usc/cs404/catchup/LocationObject.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class LocationObject implements Serializable {
 
+    String id;
     String restaurantName;
     String imageUrl;
     boolean isClosed;
@@ -20,10 +21,11 @@ public class LocationObject implements Serializable {
     double distance;
 
 
-    public LocationObject(String restaurantName, String imageUrl, boolean isClosed, int reviewCount,
+    public LocationObject(String id, String restaurantName, String imageUrl, boolean isClosed, int reviewCount,
                           ArrayList<String> categories, double rating, ArrayList<String> transactions,
                           String price, String location, double latitude, double longitude,
                           String phone, double distance) {
+        this.id = id;
         this.restaurantName = restaurantName;
         this.imageUrl = imageUrl;
         this.isClosed = isClosed;
@@ -46,12 +48,30 @@ public class LocationObject implements Serializable {
         this.restaurantName = restaurantName;
     }
 
+    public String getId() {return id;}
     public String getRestaurantName() {return restaurantName; }
     public String getImageUrl() { return imageUrl; }
     public boolean getIsClosed() { return isClosed; }
     public int getReviewCount() { return reviewCount; }
     public ArrayList<String> getCategories() { return categories; }
+    public String getCategoryString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < categories.size(); i++) {
+            if (i != 0) {
+                sb.append(", ");
+            }
+            sb.append(categories.get(i));
+        }
+        return sb.toString();
+    }
     public double getRating() { return rating; }
+    public String getRatingString() {
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < rating; j++) {
+            sb.append("*");
+        }
+        return sb.toString();
+    }
     public ArrayList<String> getTransactions() { return transactions; }
     public String getPrice() { return price; }
     public String getLocation() { return location; }
@@ -60,4 +80,25 @@ public class LocationObject implements Serializable {
     public String getPhone() { return phone; }
     public double getDistance() { return distance; }
 
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o){
+
+        if(o instanceof LocationObject){
+
+            LocationObject l = (LocationObject) o;
+            if(l.id.equals(this.id))
+            {
+                return true;
+            }
+            else return false;
+
+        }
+
+        return false;
+    }
 }
